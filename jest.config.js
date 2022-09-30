4f05c01c3edbd778c0b07c59890b57123531a5b2
Cari Palazzolo <cari.pizza@gmail.com>
2022-09-30 13:11:38 -0700

    install jest, initial config files and test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('jest').Config}
+ */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+  testPathIgnorePatterns: ['dist'],
+};
