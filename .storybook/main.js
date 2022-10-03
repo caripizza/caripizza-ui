2eed45b2b0872649572bcbf6372fad5d3ea3cb2e
Cari Palazzolo <cari.pizza@gmail.com>
2022-10-02 19:25:34 -0700

    install a11y storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,8 @@ const config = {
   ],
   addons: [
     '@storybook/addon-essentials',
-    'storybook-addon-themes'
+    'storybook-addon-themes',
+    '@storybook/addon-a11y'
   ],
   framework: '@storybook/react',
   features: {
