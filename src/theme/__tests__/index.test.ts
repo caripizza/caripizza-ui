65d9eb82dd01db744e47e3c6291578b01bbafe8d
Cari Palazzolo <cari.pizza@gmail.com>
2022-10-09 20:28:44 -0700

    refactor theme

--- src/theme/__tests__/index.test.ts
+++ src/theme/__tests__/index.test.ts
@@ -1,7 +1,11 @@
-import { theme } from '../';
+import { darkTheme, lightTheme, themes } from '../';
 
 describe('theme', () => {
-  it('should have a default theme mode', () => {
-    expect(theme.mode).toEqual('light');
+  it('should have a light theme', () => {
+    expect(themes.light).toMatchObject(lightTheme);
+  });
+
+  it('should have a dark theme', () => {
+    expect(themes.dark).toMatchObject(darkTheme);
   });
 });
