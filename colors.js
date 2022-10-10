a86f17c31a4b1affb1cb796f24e062a4498ff4c2
Cari Palazzolo <cari.pizza@gmail.com>
2022-10-09 20:24:25 -0700

    refactor colors

--- colors.js
+++ colors.js
@@ -1,12 +1,16 @@
 /* UI Colors for tailwind.config.js, utility classes */
 module.exports = {
-  aqua: {
-    50: 'rgb(var(--color-aqua) / 0.5)',
-    100: 'rgb(var(--color-aqua) / 1)',
+  turquoise: {
+    25: 'rgb(var(--color-turquoise) / 0.25)',
+    50: 'rgb(var(--color-turquoise) / 0.5)',
+    75: 'rgb(var(--color-turquoise) / 0.75)',
+    100: 'rgb(var(--color-turquoise) / 1)',
   },
-  fuschia: {
-    50: 'rgb(var(--color-fuschia) / 0.5)',
-    100: 'rgb(var(--color-fuschia) / 1)',
+  gold: {
+    25: 'rgb(var(--color-gold) / 0.25)',
+    50: 'rgb(var(--color-gold) / 0.5)',
+    75: 'rgb(var(--color-gold) / 0.75)',
+    100: 'rgb(var(--color-gold) / 1)',
   },
   primary: 'rgb(var(--text-primary) / 1)',
   'backg-primary': 'rgb(var(--backg-primary) / 1)',
