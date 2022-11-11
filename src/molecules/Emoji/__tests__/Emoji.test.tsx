a5e8ca0d04adc7cc017c82a02e53c0e588bce156
Cari Palazzolo <cari.pizza@gmail.com>
2022-11-11 12:50:42 -0800

    fix tests

--- src/molecules/Emoji/__tests__/Emoji.test.tsx
+++ src/molecules/Emoji/__tests__/Emoji.test.tsx
@@ -4,8 +4,8 @@ import { Emoji, EmojiProps, renderEmoji, findEmoji } from '..';
 import { emojis } from '../resources';
 
 describe('Emoji', () => {
-  const renderEl = ({ shortName, ...props }: EmojiProps) => {
-    const utils = render(<Emoji {...props} shortName={shortName} />);
+  const renderEl = (props?: EmojiProps) => {
+    const utils = render(<Emoji {...props} shortName={props?.shortName} />);
     const span = screen.getByRole('img');
     return { ...utils, span };
   };
@@ -19,9 +19,25 @@ describe('Emoji', () => {
     expect(span.innerHTML).toEqual(renderEmoji(shortName));
   });
 
+  it('should render with aria-label based on found emoji short-name', () => {
+    const { span } = renderEl({ shortName: 'detective' });
+    expect(span.getAttribute('aria-label')).toEqual('detective');
+  });
+
+  it('should render no aria-label if no short-name provided', () => {
+    const { span } = renderEl();
+    expect(span).not.toHaveAttribute('aria-label');
+  });
+
   describe('findEmoji helper', () => {
     it('should take a short-name and return a matching emoji', () => {
       expect(findEmoji('cat')).toEqual(emoji);
+    });
+  });
+
+  describe('renderEmoji helper', () => {
+    it('should return undefined without a short-name', () => {
+      expect(renderEmoji(undefined)).toBeUndefined();
     });
   });
 });
